a5a45893989e63ee2374b5c2c73d38fc4f51ab45
Lakshmipathi.G <Lakshmipathi.G@giis.co.in>
2017-01-13 17:37:56 +0530

    finding volume of cylinder

---
+++ 0006_volume_of_cylinder.rs
@@ -0,0 +1,9 @@
+// volume of cylinder
+fn main() {
+    let r: f32 = 5.0;
+    let pi: f32 = 3.14;
+    let h: f32 = 6.0;
+    let v: f32; 
+    v =  pi * ( r * r) * h;
+    println!("Result = {} ", v);
+} 
